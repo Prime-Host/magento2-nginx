751d385ac35c2c4c1ba9ae3de9f07c8c2071afd5
Prime-Host <knordloh@nordloh-webdesign.de>
2017-12-04 13:15:28 +0100

    only insert cronjob in new magento

--- magento2-start.sh
+++ magento2-start.sh
@@ -18,6 +18,14 @@ if [ ! -f /usr/share/nginx/www/app/etc/env.php ]; then
 cd /usr/share/nginx/ \
     && git clone -b 2.2 https://github.com/magento/magento2.git www \
     && rm -r www/.git
+
+# insert cronjob
+sudo -u $PRIMEHOST_USER bash << EOF
+crontab -l | { cat; echo "* * * * * php /usr/share/nginx/www/bin/magento cron:run | grep -v 'Ran jobs by schedule' >> /usr/share/nginx/www/var/log/magento.cron.log"; } | crontab -
+crontab -l | { cat; echo "* * * * * php /usr/share/nginx/www/update/cron.php >> /usr/share/nginx/www/var/log/update.cron.log"; } | crontab -
+crontab -l | { cat; echo "* * * * * php /usr/share/nginx/www/bin/magento setup:cron:run >> /usr/share/nginx/www/var/log/setup.cron.log"; } | crontab -
+EOF
+
 fi
 
 # set custom user for magento files
@@ -29,13 +37,6 @@ chown -R $PRIMEHOST_USER:$PRIMEHOST_USER /var/lib/php/sessions/
 # composer install
 cd /usr/share/nginx/www
 su -c "composer install" -m "$PRIMEHOST_USER"
-
-# insert cronjob
-sudo -u $PRIMEHOST_USER bash << EOF
-crontab -l | { cat; echo "* * * * * php /usr/share/nginx/www/bin/magento cron:run | grep -v 'Ran jobs by schedule' >> /usr/share/nginx/www/var/log/magento.cron.log"; } | crontab -
-crontab -l | { cat; echo "* * * * * php /usr/share/nginx/www/update/cron.php >> /usr/share/nginx/www/var/log/update.cron.log"; } | crontab -
-crontab -l | { cat; echo "* * * * * php /usr/share/nginx/www/bin/magento setup:cron:run >> /usr/share/nginx/www/var/log/setup.cron.log"; } | crontab -
-EOF
 
 # start all services
 /usr/local/bin/supervisord -n -c /etc/supervisord.conf
