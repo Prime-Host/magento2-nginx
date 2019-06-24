67f2eb8447e36fb44645771ea67a814719d55212
Prime Host <gregorius.stahlbart@gmail.com>
2019-06-24 15:33:19 +0200

    Update magento2-start.sh

--- magento2-start.sh
+++ magento2-start.sh
@@ -37,7 +37,7 @@ SHELL=/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
 * * * * * perl -le 'sleep rand 40' && php /usr/share/nginx/www/bin/magento cron:run | grep -v 'Ran jobs by schedule' >> /usr/share/nginx/www/var/log/magento.cron.log && php /usr/share/nginx/www/update/cron.php >> /usr/share/nginx/www/var/log/update.cron.log && php /usr/share/nginx/www/bin/magento setup:cron:run >> /usr/share/nginx/www/var/log/setup.cron.log
-* * * * * mysqldump -u"root" -p"$PRIMEHOST_PASSWORD" -h"$PRIMEHOST_DOMAIN"-db magento2 > $HOME/backup/dump.sql"; } | crontab -
+* * * * * mysqldump -u"root" -p"$PRIMEHOST_PASSWORD" -h"$PRIMEHOST_DOMAIN"-db magento2 > /home/$PRIMEHOST_USER/backup/dump.sql"; } | crontab -
 EOF
 fi
 
